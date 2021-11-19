cc14f0ebe57700d0c3c34b00931c1d3d127477b7
赵云潇 <zhao.yunxiao@outlook.com>
2021-11-19 17:41:37 +0800

    litte test

--- playground.go
+++ playground.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
+	fmt.Println(os.Args[0])
+	loopTest()
+}
+
+func loopTest() {
+	i := 1
+	for {
+		if i == 10 {
+			break
+		} else {
+			i++
+		}
+	}
+	fmt.Println("Loop finished.")
 }
