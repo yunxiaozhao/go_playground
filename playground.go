c2fea70a0e87d4c5ef7726b648b93eda26debb48
赵云潇 <zhao.yunxiao@outlook.com>
2021-12-01 21:25:06 +0800

    .

--- playground.go
+++ playground.go
@@ -8,5 +8,17 @@ import (
 func main() {
 	fmt.Println("Hello, world!")
 	fmt.Println(os.Args[0])
-	LoopTest()
+	fmt.Println(-28 >> 12)
+	fmt.Printf("%d %o %[1]o\n", 1, 2) // "438 666 0666"
+	s := "hello, world"
+	fmt.Println(len(s)) // "12"
+	fmt.Println(comma("123123123"))
+}
+
+func comma(s string) string {
+	n := len(s)
+	if n <= 3 {
+		return s
+	}
+	return comma(s[:n-3]) + "," + s[n-3:]
 }
